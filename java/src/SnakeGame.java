67dbd5e173284d6eeab2f6759d21c6a34661b0d9
jramirez125 <Kg.0919mx>
2020-02-19 14:41:36 -0700

    created default constructor that initialized an empty 1 x 1 array

---
+++ java/src/SnakeGame.java
@@ -0,0 +1,26 @@
+public class SnakeGame {
+
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+    public int[][] headWhere;
+
+    public SnakeGame(){
+        this.game[0][0] = true;
+        }
+
+
+    
+
+
+    public void partSnake(int x){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
