665d4c84077a4c7f84e8dcc0b4b5cb4de9ab907b
jramirez125 <Kg.0919mx>
2020-02-22 18:59:34 -0700

    exhaustive checks first commit

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -5,22 +5,40 @@ public class SnakeGame {
     private static int exhaustiveChecks;
     private static int recursiveChecks;
     public int[][] headWhere;
+    public int[] headX;
+    public int[] headY;
 
-    public SnakeGame(){
-        this.game[0][0] = true;
-        }
-
-
-
-
-
-    public void partSnake(int x){
-
+    public SnakeGame() {
+        this.game = new boolean[0][0];
     }
 
+    public SnakeGame(boolean[][] game, int[] headX, int[] headY) {
+        this.game = game;
+        this.headX = headX;
+        this.headY = headY;
+    }
+
+    public int[] findTailExhaustive(){
+        int exhaustiveChecks = 0;
+        int partOfSnake = 0;
+        int[] tailCheck = new int[3];    //creates an array tailCheck[X][y][SNAKE LENGTH]
+        tailCheck[0] = headX[0];         //tailCheck[x][][]
+        tailCheck[1] = headY[0];         //tailCheck[][Y][]
+        for (int i = 0; i < game.length; i++) {
+            for (int k = 0; k < game[i].length; k++) {
+                exhaustiveChecks++;
+                if(game[i][k] == game[headX[0]][headY[0]]){
+
+                    tailCheck[2] = exhaustiveChecks;
+                    return tailCheck;
+                }
+            }
+        }
+        return tailCheck;
+    }
 
     public static void main(String[] args) {
 
     }
-
 }
+
