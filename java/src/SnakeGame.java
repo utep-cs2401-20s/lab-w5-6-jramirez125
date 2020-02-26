a92f5d852cf567867d4ed07dc969d35bc95697af
jramirez125 <Kg.0919mx>
2020-02-26 14:49:30 -0700

    modified exhaustive check using a while loop

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -4,46 +4,60 @@ public class SnakeGame {
     private int[] headPosition;
     private static int exhaustiveChecks;
     private static int recursiveChecks;
-    public int[][] headWhere;
-    public int[] headX = new int[1];
-    public int[] headY = new int[1];
-    int width;
-    int height;
 
-    public void SnakeGame(){
-        game = new boolean[1][1];
+
+    public SnakeGame() {
+        game = new boolean[0][0];
     }
 
+    int headX = 0;
+    int headY = 0;
+
     public SnakeGame(boolean[][] game, int[] headX, int[] headY) {
-        this.game = game;
-        this.headX = headX;
-        this.headY = headY;
+        game = new boolean[game.length][game[0].length];
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+                this.game[i][j] = game[i][j];
+            }
+        }
+        headPosition[0] = this.headX;
+        headPosition[1] = this.headY;
     }
 
 
     public int[] findTailExhaustive() {
-        int exhaustiveChecks = 0;
-        int[] head1 = new int[2];
+        resetCounters();
+        int[] snake1 = new int[3];
         int[] tailCheck = new int[3];//creates an array tailCheck[X][y][SNAKE LENGTH]
-
         for (int i = 0; i < game.length; i++) {
             for (int k = 0; k < game[i].length; k++) {
-                exhaustiveChecks++;
-                if (game[i][k] == game[headX[0]][headY[0]]) {
 
-                    head1[0] = headX[0];
-                    head1[1] = headY[0];
+                while (game[i][k] = true) {
+                    exhaustiveChecks++;
                 }
+                snake1[0] = headX;
+                snake1[1] = headY;
+                snake1[2] = exhaustiveChecks;
             }
-            tailCheck[0] = headX[0];
-            tailCheck[1] = headY[0];
-            tailCheck[2] = exhaustiveChecks;
         }
         return tailCheck;
     }
 
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
     public static void main(String[] args) {
-        
+    boolean[][] gameBoard = new boolean[5][5];
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int k = 0; k < gameBoard.length; k++) {
+                gameBoard[i][k] = false;
+            }
+        }
+
+        SnakeGame run = new SnakeGame(gameBoard, new int[2], new int[2]);
+        run.findTailExhaustive();
     }
 }
 
